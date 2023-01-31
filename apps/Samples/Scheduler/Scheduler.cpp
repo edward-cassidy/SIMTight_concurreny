44a388900d52b88a93189ff6389b37eb361d2fb3
Eddie Cassidy <edward.cassidy@hotmail.com>
2023-01-31 11:40:19 +0000

    BUG: The scheduler will not run from it's own file

---
+++ apps/Samples/Scheduler/Scheduler.cpp
@@ -0,0 +1,157 @@
+#include <NoCL.h>
+#include <Rand.h>
+
+
+template <int SquareSize> struct Transpose : Kernel {
+  Array2D<int> in, out;
+  
+  void kernel() {
+    auto square = shared.array<int, SquareSize, SquareSize+1>();
+    
+    // Origin of square within matrix
+    int originX = blockIdx.x * blockDim.x;
+    int originY = blockIdx.y * blockDim.x;
+    
+    // Load square
+    for (int y = threadIdx.y; y < blockDim.x; y += blockDim.y)
+      square[y][threadIdx.x] = in[originY + y][originX + threadIdx.x];
+    
+    __syncthreads();
+    
+    // Store square
+    for (int y = threadIdx.y; y < blockDim.x; y += blockDim.y)
+      out[originX + y][originY + threadIdx.x] = square[threadIdx.x][y];
+  }
+};
+
+class FixedQueue {
+    public:
+
+        Kernel **arr;
+        int head;
+        int tail;
+        int size;
+
+        FixedQueue(Kernel **array, int size_): head(0), tail(0), arr(array), size(size_){}
+
+        Kernel * pop(){
+            int temp = head;
+            head = (head + 1)%size;
+            return arr[temp];
+        }
+
+        void enqueue(Kernel *k){
+            arr[tail] = k;
+            tail = (tail+1)%size;
+        }
+        
+};
+
+int scheduler(Kernel **arr, int size){
+    
+    FixedQueue queue(arr , size);
+    puts("Creating Queue");
+    for (int i = 0; i < size; i++)
+    {
+        queue.enqueue(arr[i]);
+    }
+
+    puts("Finshed creating Queue");
+    bool kernel_finished = false;
+    while (true)
+    {
+        puts("Running kernel");
+        kernel_finished = false;
+        Kernel *k = queue.pop();
+        if(k->blockIdx.y < k->gridDim.y){
+            if(k->blockIdx.x < k->gridDim.x){
+                go_func(k);
+                k->blockIdx.x += k->map.numXBlocks;
+            }else{
+                k->blockIdx.x = 0;
+                k->blockIdx.y += k->map.numYBlocks;
+            }
+        }else{
+            kernel_finished = true;
+        }
+
+        if(!kernel_finished){
+            queue.enqueue(k);
+        }else if(queue.head == queue.tail){
+            break;  
+        }
+    }
+    return 0;
+}
+
+
+int main()
+{
+  puts("start");
+  int width = 256;
+  int height = 64;
+
+  // Input and output matrix data
+  nocl_aligned int matInData[width*height];
+  nocl_aligned int matOutData[width*height];
+
+  // Friendly array wrappers
+  Array2D<int> matIn(matInData, height, width);
+  Array2D<int> matOut(matOutData, width, height);
+
+  // Initialise inputs
+  uint32_t seed = 1;
+  for (int i = 0; i < height; i++)
+    for (int j = 0; j < width; j++)
+      matIn[i][j] = i*256 + j;
+
+  // Number of loop iterations per block.  The number of iterations
+  // times the block Y dimension must equal the block X dimension.
+  const int itersPerBlock = 4;
+
+  // Instantiate kernel
+  Transpose<SIMTLanes> k;
+
+  // Set block/grid dimensions
+  k.blockDim.x = SIMTLanes;
+  k.blockDim.y = SIMTLanes / itersPerBlock;
+  k.gridDim.x = width / k.blockDim.x;
+  k.gridDim.y = height / (itersPerBlock * k.blockDim.y);
+
+  //Assign thread IDs
+
+
+  k.blockIdx.x = 0;
+  k.blockIdx.y = 0;
+
+
+  // Assign parameters
+  k.in = matIn;
+  k.out = matOut;
+
+  puts("before mapping");
+  mapping_func<Transpose<SIMTLanes>>(&k);
+  Kernel *array[1] = {&k};
+  puts("Before Scheduler");
+  scheduler(array, 1);
+  puts("After Scheduler");
+
+  bool ok = true;
+  bool check = false;
+  for (int i = 0; i < width; i++){
+    for (int j = 0; j < height; j++){
+      ok = ok && matOut[i][j] == matIn[j][i];
+    }
+  }
+    
+
+  // Display result
+  puts("Self test: ");
+  puts(ok ? "PASSED" : "FAILED");
+  putchar('\n');
+  return 0;
+ 
+}
+
+
+
